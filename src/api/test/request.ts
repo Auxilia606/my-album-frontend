7968bd5275de92400ae846129fd37b0c54083c90
DESKTOP-61B6P5C\auxil <auxilia606@gmail.com>
2024-02-28 14:08:11 +0900

    feat(submodule): type submodule 연결

--- src/api/test/request.ts
+++ src/api/test/request.ts
@@ -1,7 +1,8 @@
+import { UserDTO } from "@types";
 import { defaultApi } from "../invoke";
 
 export const getTest = async () => {
-  const { data } = await defaultApi.get("/test");
+  const { data } = await defaultApi.get<UserDTO>("/user");
 
   return data;
 };
